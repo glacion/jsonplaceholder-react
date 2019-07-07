1cf715ba7b3bba34a010ba1478b6505da34f9903
Ahmetcan Güvendiren <ahm@glacion.com>
2019-07-07 19:34:29 +0300

    Pick a darker primary color

--- src/component/container/Theme.jsx
+++ src/component/container/Theme.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
-import red from '@material-ui/core/colors/red';
 import { CssBaseline } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
 const theme = createMuiTheme({
   palette: {
     type: 'dark',
-    primary: red,
+    primary: {
+      main: '#7f0000',
+    },
   },
 });
 
